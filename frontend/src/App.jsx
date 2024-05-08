0cbc129549600bd9dc53391ea2b6fd88b1b2e829
pazitto2208 <epazitto@gmail.com>
2024-05-08 18:19:44 +0200

    first steps with react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+
+export default function App() {
+	return (
+		<>
+			<h1 style={{ background: 'red', width: 'fit-content', padding: '200px' }}>My Gastronomy</h1>
+			<p>Fala galera do canal, beleza?</p>
+		</>
+	)
+}
